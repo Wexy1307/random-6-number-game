2730fc71a63f120fd133be9365795c5ac43f9a2f
Wexy1307 <145032136+Wexy1307@users.noreply.github.com>
2023-09-21 16:55:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+a = random.randint(0, 9)
+b = random.randint(0, 9)
+c = random.randint(0, 9)
+d = random.randint(0, 9)
+e = random.randint(0, 9)
+f = random.randint(0, 9)
+
+print(a, b, c, d, e, f, sep='')
+# print(a, b, c, d, e, f, sep='+') en dan z.s.m. oplossen is ook een idee #
